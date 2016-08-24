77e1ac0a8ea56d90392af857e9673f98ee40964d
seiyria <kyle@seiyria.com>
2016-08-24 17:28:07 -0500

    fixing collectible display

--- test/list/collectible.js
+++ test/list/collectible.js
@@ -9,7 +9,7 @@ console.log(`Collectible List (${collectibleObjs.length})`);
 
 _.each(_.sortBy(collectibleObjs, 'name'), obj => {
   var storyline = obj.properties ? obj.properties.storyline : obj.storyline;
-  console.log(obj.name, '\t|\t', storyline, '\t|\t', obj.origin);
+  console.log(_.padStart(obj.name, 35), '\t|\t', _.padStart(storyline, 25), '\t|\t', obj.origin);
 });
 
 console.log("travis_fold:end:list_collectibles");
