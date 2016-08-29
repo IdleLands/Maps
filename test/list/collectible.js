358e19c6e403359b55ae684c762af5fa1ab0a5a0
seiyria <kyle@seiyria.com>
2016-08-29 16:22:21 -0500

    adding collectible rarity to output

--- test/list/collectible.js
+++ test/list/collectible.js
@@ -9,7 +9,8 @@ console.log(`Collectible List (${collectibleObjs.length})`);
 
 _.each(_.sortBy(collectibleObjs, 'name'), obj => {
   var storyline = obj.properties ? obj.properties.storyline : obj.storyline;
-  console.log(_.padStart(obj.name, 35), '\t|\t', _.padStart(storyline, 25), '\t|\t', obj.origin);
+  var rarity = (obj.properties ? obj.properties.rarity : obj.rarity) || 'basic';
+  console.log(_.padStart(obj.name, 35), '\t|\t', _.padStart(rarity), '\t|\t', _.padStart(storyline, 25), '\t|\t', obj.origin);
 });
 
 console.log("travis_fold:end:list_collectibles");
