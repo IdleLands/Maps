b68a50316777c8402e51b34ccadc07170cd855c1
Kyle J. Kemp <kyle@seiyria.com>
2017-10-29 12:53:08 -0500

    Create regions.js

---
+++ test/list/regions.js
@@ -0,0 +1,15 @@
+console.log("travis_fold:start:list_regions");
+
+const _ = require('lodash');
+const dataAggregator = require("../data-aggregator");
+const maps = dataAggregator.maps;
+
+console.log("Regions");
+let total = 0;
+_.each(maps, (mapData, mapName) => {
+  _.each(mapData.map.layers[3].objects, (obj) => {
+    console.log(`Region "${obj.name}" (#${++total})`);
+  });
+});
+
+console.log("travis_fold:end:list_regions");
